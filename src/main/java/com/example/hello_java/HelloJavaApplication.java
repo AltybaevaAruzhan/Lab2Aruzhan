c9a59f32d97221c2b1f0c442cf9dc3ca9ee3a1c8
Aruzhan Altybaeva <aruzhan.altybaeva15@gmail.com>
2024-10-04 12:29:41 +0600

    first commit

--- src/main/java/com/example/hello_java/HelloJavaApplication.java
+++ src/main/java/com/example/hello_java/HelloJavaApplication.java
@@ -1,5 +1,5 @@
 package com.example.hello_java;
-
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.example.hello_java")
--- src/main/java/com/example/hello_java/HelloWorldController.java
+++ src/main/java/com/example/hello_java/HelloWorldController.java
@@ -1,12 +1,64 @@
 package com.example.hello_java;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Random;
 import java.time.LocalDateTime;
 @RestController
 
 public class HelloWorldController {
-    @GetMapping("/helloWorld")
-    public String helloWorld() {
-        return "Hello World";
+    @GetMapping("now")
+    public String now(){
+        LocalDateTime now = LocalDateTime.now();
+        return now.toString();
     }
+    @GetMapping ("/time")
+    public String time(){
+        LocalTime now = LocalTime.now();
+        return now.toString();
+    }
+    @GetMapping("/month")
+    public String month(){
+        LocalDateTime now = LocalDateTime.now();
+        int month = now.getMonthValue();
+        return String.valueOf(month);
+    }
+    @GetMapping("/api")
+    public String numberString(@RequestParam int a) {
+        StringBuilder c = new StringBuilder();
+
+        for (int i = 1; i <= a; i++) {
+            c.append(i);
+            if (i < a) {
+                c.append(", ");
+            }
+        }
+        return c.toString();
+    }
+    @GetMapping("/random_number")
+    public int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(500) + 1;
+    }
+    @GetMapping ("fibi")
+    public int fibi(@RequestParam int a) {
+        if(a == 1 || a == 2) {
+            return 1;
+        }
+        int[] fib = new int[a];
+        fib[0] = 1;
+        fib[1] = 1;
+        for (int i = 2; i < a; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[a-1];
+    }
+    @GetMapping("/reverse/{string}")
+    public String reverse(@PathVariable String string) {
+        return new StringBuilder(string).reverse().toString();
+
+    }
+
+
+
 }
